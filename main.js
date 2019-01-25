ca6f62a471418c3fa4d70771f8d16d8553af920f
SnFoil <samwitte@me.com>
2019-01-24 19:21:53 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
 var gameData = {
-	gold: 0,
-	goldPerClick: 1,
-	goldPerClickCost: 10
+	ore: 0,
+	orePerClick: 1,
+	orePerClickCost: 10,
+  drill: 0,
+  drillCost: 100,
+  furnace: 0,
+  furnaceCost: 1000
+
 }
 
-function mineGold() {
-  gameData.gold += gameData.goldPerClick
-  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+function mineOre() {
+  gameData.ore += gameData.orePerClick
+  document.getElementById("oreMined").innerHTML = gameData.ore + " Gold Ore"
 }
 
-function buyGoldPerClick() {
-  if (gameData.gold >= gameData.goldPerClickCost) {
-    gameData.gold -= gameData.goldPerClickCost
-    gameData.goldPerClick += 1
-    gameData.goldPerClickCost *= 2
-    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
-    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+function buyOrePerClick() {
+  if (gameData.ore >= gameData.orePerClickCost) {
+    gameData.ore -= gameData.orePerClickCost
+    gameData.orePerClick += 1
+    gameData.orePerClickCost *= 2
+    document.getElementById("oreMined").innerHTML = gameData.ore + " Gold Ore"
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.orePerClick + ") Cost: " + gameData.orePerClickCost + " Ore"
+  }
+}
+
+function buyDrill() {
+	if (gameData.ore >= gameData.drillCost) {
+    gameData.ore -= gameData.drillCost
+    gameData.drill += 1
+    gameData.drillCost *= 2
+    document.getElementById("oreMined").innerHTML = gameData.ore + " Gold Ore"
+    document.getElementById("drillUpgrade").innerHTML = "Buy a drill (Currently own " + gameData.drill + ") Cost: " + gameData.drillCost + " Ore"
   }
 }
 
 var mainGameLoop = window.setInterval(function() {
-  mineGold()
-}, 1000)
+  mineOre()
+}, 864000000)
 
 var saveGameLoop = window.setInterval(function() {
-  localStorage.setItem('goldMinerSave', JSON.stringify(gameData))
-}, 15000)
+  localStorage.setItem('oreMinerSave', JSON.stringify(gameData))
+}, 10000)
 
-var savegame = JSON.parse(localStorage.getItem("goldMinerSave"))
+var savegame = JSON.parse(localStorage.getItem("oreMinerSave"))
 if (savegame !== null) {
   gameData = savegame
 }
