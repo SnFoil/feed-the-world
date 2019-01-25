8c99512b4356b50cfa2cb06f01aa8a8f8e3c8f68
chaddybox026 <chaddybox026@gmail.com>
2019-01-25 09:06:22 -0600

    Workaround for reset button

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+var defaultData = {
+  ore: 0, //how much ore the player has
+  orePerClick: 1, //ore recieved per button press
+  orePerClickCost: 10, //cost to upgrade orePerClick
+  drill: 0, //how many drills the player has
+  drillCost: 100, //how much it costs to purchase another drill
+  furnace: 0, //how many furnaces the player has
+  furnaceCost: 1000, //how much it costs to purchase another furnace
+  autoMine: 1000 //autoMine is the time, in ms, that the player recieves gold.
+}
 
-//all variables
+//game variables
 var gameData = {
   ore: 0, //how much ore the player has
   orePerClick: 1, //ore recieved per button press
@@ -14,10 +24,7 @@ var gameData = {
   document.getElementById("oreSmelt").style.display = "none" //sets smelt ore into ingots button to be invisible before the player has 50 ore
 
 function developerReset() { //developer reset button
-  gameData.ore = 0
-  gameData.drill = 0
-  gameData.furnace = 0
-  gameData.orePerClick = 1
+  gameData = default
   document.getElementById("oreMined").innerHTML = gameData.ore + " Gold Ore"
   document.getElementById("goldIngots").innerHTML = gameData.gold + " Gold Ingots"
   document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.orePerClick + ") Cost: " + gameData.orePerClickCost + " Ore"
