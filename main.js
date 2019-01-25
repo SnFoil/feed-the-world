e55969f507be11d750523a175c2027f93556e4a7
chaddybox026 <chaddybox026@gmail.com>
2019-01-25 09:40:11 -0600

    Fixed Reset Button
    setting object equal to object causes the object to become the object

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-var defaultData = {
-  ore: 0, //how much ore the player has
-  ingot: 0, //how much ingot the player has
-  orePerClick: 1, //ore recieved per button press
-  orePerClickCost: 10, //cost to upgrade orePerClick
-  drill: 0, //how many drills the player has
-  drillCost: 100, //how much it costs to purchase another drill
-  furnace: 0, //how many furnaces the player has
-  furnaceCost: 1000, //how much it costs to purchase another furnace
-  autoMine: 1000 //autoMine is the time, in ms, that the player recieves gold.
-}
 
 //game variables
 var gameData = {
@@ -26,7 +15,18 @@ var gameData = {
   document.getElementById("oreSmelt").style.display = "none" //sets smelt ore into ingots button to be invisible before the player has 50 ore
 
 function developerReset() { //developer reset button
-  gameData = defaultData
+  gameData = {
+    ore: 0, //how much ore the player has
+    ingot: 0, //how much ingot the player has
+    orePerClick: 1, //ore recieved per button press
+    orePerClickCost: 10, //cost to upgrade orePerClick
+    drill: 0, //how many drills the player has
+    drillCost: 100, //how much it costs to purchase another drill
+    furnace: 0, //how many furnaces the player has
+    furnaceCost: 1000, //how much it costs to purchase another furnace
+    autoMine: 1000 //autoMine is the time, in ms, that the player recieves gold.
+  }
+  
   document.getElementById("oreMined").innerHTML = gameData.ore + " Gold Ore"
   document.getElementById("goldIngots").innerHTML = gameData.ingot + " Gold Ingots"
   document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.orePerClick + ") Cost: " + gameData.orePerClickCost + " Ore"
